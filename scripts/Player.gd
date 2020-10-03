297053aa2eb39db233905d818ea7d6f85766c253
Jai2500 <jai.bardhan90@gmail.com>
2020-10-04 01:38:17 +0530

    1.Changed Project Settings to add PlayerVariables.gd on AutoLoad 2. Added Shop Menu Scene 3. Added the required functionality for the shop menu

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -13,33 +13,33 @@ var tool_level = 1
 var resource = 0
 
 func _input(event):
-    if event is InputEventMouseMotion:
-        player.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+	if event is InputEventMouseMotion:
+		player.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 
 func _process(_delta):
-    if Input.is_action_just_pressed("ui_cancel"):
-        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
-    var camera_basis = camera.get_camera_transform().basis
-    var direction = Vector3()
+	var camera_basis = camera.get_camera_transform().basis
+	var direction = Vector3()
 
-    if Input.is_action_pressed("move_forward"):
-        direction -= camera_basis.z
-    elif Input.is_action_pressed("move_backward"):
-        direction += camera_basis.z
+	if Input.is_action_pressed("move_forward"):
+		direction -= camera_basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += camera_basis.z
 
-    if Input.is_action_pressed("move_left"):
-        direction -= camera_basis.x
-    elif Input.is_action_pressed("move_right"):
-        direction += camera_basis.x
+	if Input.is_action_pressed("move_left"):
+		direction -= camera_basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += camera_basis.x
 
-    if Input.is_action_pressed("move_up"):
-        direction += camera_basis.y
-    elif Input.is_action_pressed("move_down"):
-        direction -= camera_basis.y
+	if Input.is_action_pressed("move_up"):
+		direction += camera_basis.y
+	elif Input.is_action_pressed("move_down"):
+		direction -= camera_basis.y
 
-        direction = direction.normalized()
+		direction = direction.normalized()
 
-    velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-    velocity = move_and_slide(velocity)
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity = move_and_slide(velocity)
