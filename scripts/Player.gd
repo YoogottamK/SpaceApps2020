cd644dee1b6e56192f651992e742af02ed70f807
yoogottamk <yoogottamk@outlook.com>
2020-10-03 13:50:08 +0530

    added basic welcome screen, fullscreen and mouse capture working on web

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,19 +1,17 @@
 extends KinematicBody
 
-export var speed = 10
-export var acceleration = 5
+export var speed = 5
+export var acceleration = 1
 export var mouse_sensitivity = 0.3
 
 onready var camera = $Camera
+onready var player = $"."
 
 var velocity = Vector3()
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 func _input(event):
 	if event is InputEventMouseMotion:
-		camera.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		player.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -25,6 +23,15 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_forward"):
 		direction -= camera_basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += camera_basis.z
+
+	if Input.is_action_pressed("move_left"):
+		direction -= camera_basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += camera_basis.x
+
+		direction = direction.normalized()
 
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
