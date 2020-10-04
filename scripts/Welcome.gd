bda208be2402b5e3d551207d7533f4440254648c
yoogottamk <yoogottamk@outlook.com>
2020-10-04 21:20:16 +0530

    merged welcome screen

--- scripts/Welcome.gd
+++ scripts/Welcome.gd
@@ -1,8 +1,12 @@
 extends Button
 
+onready var click_sound = $"../../ClickSound"
+
 func _on_Button_pressed():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    click_sound.play()
     get_tree().change_scene("res://scenes/World.tscn")
 
 func _on_FullscreenToggle_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
+    click_sound.play()
