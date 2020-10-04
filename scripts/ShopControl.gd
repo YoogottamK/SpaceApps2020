6464a6ee7a8f31150d5a55c0dea194f058785089
Ahish9009 <ahishjayant9009@gmail.com>
2020-10-04 16:41:20 +0400

    added to the scene

--- scripts/ShopControl.gd
+++ scripts/ShopControl.gd
@@ -6,66 +6,66 @@ const upgrade_costs = [200, 500, 1000, 5000, 10000]
 const n_upgrades = 5
 
 onready var btns = [ \
-                    $ShopUIContainer/Upgrades/BtnUpd1, \
-                    $ShopUIContainer/Upgrades/BtnUpd2, \
-                    $ShopUIContainer/Upgrades/BtnUpd3, \
-                    $ShopUIContainer/Upgrades/BtnUpd4, \
-                    $ShopUIContainer/Upgrades/BtnUpd5  \
-                   ]
+					$ShopUIContainer/Upgrades/BtnUpd1, \
+					$ShopUIContainer/Upgrades/BtnUpd2, \
+					$ShopUIContainer/Upgrades/BtnUpd3, \
+					$ShopUIContainer/Upgrades/BtnUpd4, \
+					$ShopUIContainer/Upgrades/BtnUpd5  \
+				   ]
 
 onready var current_level = $ShopUIContainer/Level/Value
 onready var max_level = $ShopUIContainer/lblMaxlevel
 
 func _ready():
-    for i in range(n_upgrades):
-        btns[i].text = "Upgrade to level " + String(i+1) + " - " +  \
-                        String(upgrade_costs[i]) + " resources"
+	for i in range(n_upgrades):
+		btns[i].text = "Upgrade to level " + String(i+1) + " - " +  \
+						String(upgrade_costs[i]) + " resources"
 
 func _process(_delta):
-    current_level.set_text(str(PlayerVariables.tool_level))
+	current_level.set_text(str(PlayerVariables.tool_level))
 
-    if PlayerVariables.tool_level < n_upgrades:
-        max_level.hide()
-    else:
-        max_level.modulate = Color(256, 256, 256, 1)
+	if PlayerVariables.tool_level < n_upgrades:
+		max_level.hide()
+	else:
+		max_level.modulate = Color(256, 256, 256, 1)
 
-    for i in range(n_upgrades):
-        if PlayerVariables.tool_level >= i + 1:
-            btns[i].modulate = Color(0, 0, 0, 0)
-            btns[i].disabled = true
-        elif PlayerVariables.resource < upgrade_costs[i]:
-            btns[i].disabled = true
-            btns[i].modulate = Color(0.5, 0.5, 0.5, 1)
-        else:
-            btns[i].disabled = false
+	for i in range(n_upgrades):
+		if PlayerVariables.tool_level >= i + 1:
+			btns[i].modulate = Color(0, 0, 0, 0)
+			btns[i].disabled = true
+		elif PlayerVariables.resource < upgrade_costs[i]:
+			btns[i].disabled = true
+			btns[i].modulate = Color(0.5, 0.5, 0.5, 1)
+		else:
+			btns[i].disabled = false
 
 
 func _on_BtnUpd1_pressed():
-    PlayerVariables.resource -= upgrade_costs[0]
-    PlayerVariables.tool_skills += upgrade_tool[0]
-    PlayerVariables.tool_level = 1
+	PlayerVariables.resource -= upgrade_costs[0]
+	PlayerVariables.tool_skills += upgrade_tool[0]
+	PlayerVariables.tool_level = 1
 
 func _on_BtnUpd2_pressed():
-    PlayerVariables.resource -= upgrade_costs[1]
-    PlayerVariables.tool_skills += upgrade_tool[1]
-    PlayerVariables.tool_level = 2
+	PlayerVariables.resource -= upgrade_costs[1]
+	PlayerVariables.tool_skills += upgrade_tool[1]
+	PlayerVariables.tool_level = 2
 
 func _on_BtnUpd3_pressed():
-    PlayerVariables.resource -= upgrade_costs[2]
-    PlayerVariables.tool_skills += upgrade_tool[2]
-    PlayerVariables.tool_level = 3
+	PlayerVariables.resource -= upgrade_costs[2]
+	PlayerVariables.tool_skills += upgrade_tool[2]
+	PlayerVariables.tool_level = 3
 
 func _on_BtnUpd4_pressed():
-    PlayerVariables.resource -= upgrade_costs[3]
-    PlayerVariables.tool_skills += upgrade_tool[3]
-    PlayerVariables.tool_level = 4
+	PlayerVariables.resource -= upgrade_costs[3]
+	PlayerVariables.tool_skills += upgrade_tool[3]
+	PlayerVariables.tool_level = 4
 
 func _on_BtnUpd5_pressed():
-    PlayerVariables.resource -= upgrade_costs[4]
-    PlayerVariables.tool_skills += upgrade_tool[4]
-    PlayerVariables.tool_level = 5
+	PlayerVariables.resource -= upgrade_costs[4]
+	PlayerVariables.tool_skills += upgrade_tool[4]
+	PlayerVariables.tool_level = 5
 
 func _on_BtnExitShop_pressed():
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    hide()
-    PlayerVariables.is_popup_open = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	hide()
+	PlayerVariables.is_popup_open = false
--- scripts/World.gd
+++ scripts/World.gd
@@ -5,20 +5,20 @@ signal show_shop
 const DebrisScene = preload("res://scenes/Debris.tscn")
 
 func _ready():
-    $Player.connect("show_shop", self, "_on_Player_show_shop")
-    $Shop.connect("show_shop", $Player, "_on_Shop_show_shop")
-    $Shop.connect("close_shop", $Player, "_on_Shop_close_shop")
-    $Timer.connect("timeout", self, "_on_Timer_timeout")
+	$Player.connect("show_shop", self, "_on_Player_show_shop")
+	$Shop.connect("show_shop", $Player, "_on_Shop_show_shop")
+	$Shop.connect("close_shop", $Player, "_on_Shop_close_shop")
+	$Timer.connect("timeout", self, "_on_Timer_timeout")
 
-    $ShopMenu.hide()
+	$ShopMenu.hide()
 
-    var new_debris = DebrisScene.instance()
-    new_debris.init(4, 3, 1.7, 1.5, 0.1)
-    new_debris.global_transform.origin = Vector3(0, 0, 0)
-    $DebrisContainer.add_child(new_debris)
+	var new_debris = DebrisScene.instance()
+	new_debris.init(4, 3, 1.7, 1.5, 0.1)
+	new_debris.global_transform.origin = Vector3(0, 0, 0)
+	$DebrisContainer.add_child(new_debris)
 
 func _on_Player_show_shop():
-    $ShopMenu.show()
+	$ShopMenu.show()
 
 func _on_Timer_timeout():
-    $Player.queue_free()
+	$Player.queue_free()
